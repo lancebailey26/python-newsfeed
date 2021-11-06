fcbde32a872dc205d4510b3b8805aa436545c6ed
Lance Bailey <lancebailey222@gmail.com>
2021-11-05 20:27:56 -0400

    added signup/login/logout + sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
+
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
@@ -15,6 +16,7 @@ def create_app(test_config=None):
     return 'hello world'
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  app.register_blueprint(api)
   app.jinja_env.filters['format_url'] = filters.format_url
   app.jinja_env.filters['format_date'] = filters.format_date
   app.jinja_env.filters['format_plural'] = filters.format_plural
